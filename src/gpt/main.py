30f3e1555cb0519818c374921dd8b377fb05b715
Fernando Pérez-García <fepegar@gmail.com>
2023-11-04 23:22:28 +0000

    Add support to run the proposed command

--- src/gpt/main.py
+++ src/gpt/main.py
@@ -1,12 +1,25 @@
 import os
+import pwd
+import subprocess
+from pathlib import Path
 from rich import print
 from typing import List
 
 import openai
 import typer
 
+try:
+    from dotenv import load_dotenv
+    load_dotenv()
+except ImportError:
+    pass
+
 
 app = typer.Typer()
+
+
+def get_shell_path() -> Path:
+    return Path(pwd.getpwuid(os.getuid()).pw_shell)
 
 
 @app.command()
@@ -17,17 +30,26 @@ def main(
         metavar="QUERY",
     ),
 ) -> None:
+    """Ask GPT-4 for a shell command.
+
+    Example:
+
+        $ ai count the number of lines in all Python files in the current directory
+    """
     query = " ".join(args)
 
     openai.api_key = os.getenv("OPENAI_API_KEY")
 
+    shell_path = get_shell_path()
+    shell_name = shell_path.name
+
     prompt = (
-        f"Write a cool zsh command (no explanations) to {query}:"
-        "\n\n```zsh\n#!/bin/zsh\n\n"
+        f"Write a {shell_name} command (no explanations) to {query}:"
+        f"\n\n```{shell_name}\n#!{shell_path}\n\n"
     )
 
     messages = [
-        {"role": "system", "content": "You are a helpful assistant, expert in zsh."},
+        {"role": "system", "content": "You are a helpful assistant, expert in shell programming."},
         {"role": "user", "content": prompt},
     ]
 
@@ -37,8 +59,19 @@ def main(
     )
 
     answer: str = completion.choices[0].message.content
-    answer = answer.strip('`').strip('\n')
+    answer = answer.strip('`').strip()
     print(answer)
+
+    print(r'Run the command? \[y/n]', end=' ')
+    while True:
+        key = typer.getchar()
+        if key == 'y':
+            print(f'\n$ {answer}')
+            result = subprocess.run(answer, capture_output=True, shell=True, text=True)
+            print(result.stdout.rstrip())
+            break
+        elif key == 'n':
+            break
 
 
 if __name__ == "__main__":
